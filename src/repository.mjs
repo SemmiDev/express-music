eaa45a6e237ee0fa69d8da1cf651423605768baf
Sammi Aldhi Yanto <sammidev4@gmail.com>
2023-07-12 18:49:08 +0700

    refactor repository

--- src/repository.mjs
+++ src/repository.mjs
@@ -1,23 +1,33 @@
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 /* 
-    {
-        id: '1',
-        title: 'Song 1',
-        artist: [
-            'Artist 1',
-            'Artist 2',
-        ],
-        url: 'URL Spotify Song'
-        totalPlay: 0,
-        createdAt: 2023-12-12T00:00:00.000Z,
-        updatedAt: 2023-12-12T00:00:00.000Z,
+  [
+   {
+      id: '1',
+      title: 'Song 1',
+      artist: [
+          'Artist 1',
+          'Artist 2',
+      ],
+      url: 'URL Spotify Song'
+      totalPlay: 0,
+      createdAt: 2023-12-12T00:00:00.000Z,
+      updatedAt: 2023-12-12T00:00:00.000Z,
     }
+  ]
 */
 
 const playlist = [];
 
-export const createSong = ({ id, title, artist, url, totalPlay, createdAt, updatedAt }) => {
+const createSong = ({
+  id,
+  title,
+  artist,
+  url,
+  totalPlay,
+  createdAt,
+  updatedAt,
+}) => {
   const song = {
     id,
     title,
@@ -28,29 +38,29 @@ export const createSong = ({ id, title, artist, url, totalPlay, createdAt, updat
     updatedAt,
   };
 
-  playlist.push(song);
+  playlist.push({ ...song });
   return song;
 };
 
-export const getSongById = (id) => playlist.find((song) => song.id === id);
+const getSongById = (id) => playlist.find((song) => song.id === id);
 
-export const getSong = (sortBy) => {
+const getSong = (sortBy) => {
   let sortedPlaylist = [...playlist];
 
   switch (sortBy) {
-    case 'createdAt':
+    case "createdAt":
       sortedPlaylist.sort((a, b) => a.createdAt - b.createdAt);
       break;
-    case 'updatedAt':
+    case "updatedAt":
       sortedPlaylist.sort((a, b) => a.updatedAt - b.updatedAt);
       break;
-    case 'createdAtDesc':
+    case "createdAtDesc":
       sortedPlaylist.sort((a, b) => b.createdAt - a.createdAt);
       break;
-    case 'updatedAtDesc':
+    case "updatedAtDesc":
       sortedPlaylist.sort((a, b) => b.updatedAt - a.updatedAt);
       break;
-    case 'totalPlay':
+    case "totalPlay":
       sortedPlaylist.sort((a, b) => b.totalPlay - a.totalPlay);
       break;
     default:
@@ -60,39 +70,36 @@ export const getSong = (sortBy) => {
   return sortedPlaylist;
 };
 
-export const playSong = (id) => {
-  const song = getSongById(id);
-  if (song) {
+const playSong = (id) => {
+  const index = playlist.findIndex((song) => song.id === id);
+
+  if (index !== -1) {
+    const song = playlist[index];
     song.totalPlay = song.totalPlay + 1;
+    playlist[index] = { ...song };
+    return song.url;
   }
 
-  playlist[playlist.findIndex((song) => song.id === id)] = song;
-
-  return song.url
+  return null;
 };
 
-export const updateSong = (id, updatedSong) => {
+const updateSong = (id, updatedSong) => {
   const index = playlist.findIndex((song) => song.id === id);
   if (index !== -1) {
     playlist[index] = { ...playlist[index], ...updatedSong };
   }
 };
 
-export const deleteSong = (id) => {
-  const index = playlist.findIndex((song) => song.id === id);
-  if (index !== -1) {
-    playlist.splice(index, 1);
-  }
+const deleteSong = (id) => {
+  playlist = playlist.filter((song) => song.id !== id);
 };
 
 export const seedPlaylist = () => {
   const song = {
     id: uuidv4(),
     title: `Naik Kereta Api`,
-    artist: [
-      `Agnes Mo`
-    ],
-    url: 'https://open.spotify.com/track/59B6XA5N4lOXPP5a0TxRSZ',
+    artist: [`Agnes Mo`],
+    url: "https://open.spotify.com/track/59B6XA5N4lOXPP5a0TxRSZ",
     totalPlay: 2,
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -109,4 +116,4 @@ export const songRepository = {
   playSong,
   updateSong,
   deleteSong,
-}
+};
